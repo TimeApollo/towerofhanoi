c9c9c2d1b3a043fc7ac0c457af3614359f512c9c
bschue <bschuess3@gmail.com>
2018-05-07 15:03:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let startTower = document.getElementById("start");
+let offsetTower = document.getElementById("offset");
+let endTower = document.getElementById("end");
+let currentClick = "clickGrab";
+let nextClick = "clickPlace";
+let towers = document.querySelectorAll("section");
+let topDiskHolder = document.getElementById("topDiskHolder");
+let info = document.getElementById("info");
+info.textContent = "Pick Piece";
+handleClick = function (event) {
+    info.textContent = "";
+    let tower = event.currentTarget;
+    console.log(tower);
+    if (currentClick === "clickGrab") {
+        if (tower.childElementCount === 0) {
+            return;
+        }
+        let topDisk = tower.lastElementChild;
+        topDiskHolder.appendChild(topDisk);
+        nextClick = "clickPlace";
+        info.textContent = "Place Piece";
+        currentClick = nextClick;
+        nextClick = "clickGrab";
+    } else {
+        if (tower.childElementCount === 0) {
+            tower.appendChild(topDiskHolder.lastElementChild);
+            info.textContent = "Pick Piece";
+            currentClick = nextClick;
+            nextClick = "clickPlace";
+        } else if (tower.lastElementChild.offsetWidth > topDiskHolder.lastElementChild.offsetWidth) {
+            tower.appendChild(topDiskHolder.lastElementChild);
+            info.textContent = "Pick Piece";
+            currentClick = nextClick;
+            nextClick = "clickPlace";
+        } else {
+            info.textContent = "Invalid Move";
+        }
+        console.log(tower.lastElementChild.offsetWidth);
+        nextClick = "clickGrab";
+
+    }
+    win();
+};
+
+function win() {
+    if (endTower.childElementCount === 4) {
+        info.textContent = "You Win!"
+    }
+}
+
+function reset() {
+    startTower.appendChild(document.getElementById("four"));
+    startTower.appendChild(document.getElementById("three"));
+    startTower.appendChild(document.getElementById("two"));
+    startTower.appendChild(document.getElementById("one"));
+}
+document.getElementById("reset").addEventListener('click', reset)
+for (let i = 0; i < towers.length; i++) {
+    towers[i].addEventListener('click', handleClick)
+};
